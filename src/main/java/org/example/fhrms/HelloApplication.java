1c9ec4c94542a1b597a136654af227a156fdb8ec
Moses <moseschizaram8@gmail.com>
2024-11-23 08:13:48 +0100

    Add the main controller to start up the admin

--- src/main/java/org/example/fhrms/HelloApplication.java
+++ src/main/java/org/example/fhrms/HelloApplication.java
@@ -1,28 +1,27 @@
-// package org.example.fhrms;
+package org.example.fhrms;
 
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Scene;
-// import javafx.scene.control.Label;
-// import javafx.scene.layout.AnchorPane;
-// import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// public class HelloApplication extends Application {
-// @Override
-// public void start(Stage stage) throws IOException {
-// FXMLLoader fxmlLoader = new
-// FXMLLoader(HelloApplication.class.getResource("admin/admin-view.fxml"));
-// Scene scene = new Scene(fxmlLoader.load(), 640, 320);
-// stage.setTitle("FHRMS!");
-// stage.setScene(scene);
-// stage.setFullScreen(true);
-// stage.show();
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("admin/admin-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 320);
+        stage.setTitle("FHRMS!");
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
 
-// }
+    }
 
-// public static void main(String[] args) {
-// launch();
-// }
-// }
+    public static void main(String[] args) {
+        launch();
+    }
+}
