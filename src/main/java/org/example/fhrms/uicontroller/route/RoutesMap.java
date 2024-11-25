e2b8fd182c0cc4448a7d7dc18dfb41643de30067
Moses <moseschizaram8@gmail.com>
2024-11-25 11:25:51 +0100

    Add route to admin analytics

--- src/main/java/org/example/fhrms/uicontroller/route/RoutesMap.java
+++ src/main/java/org/example/fhrms/uicontroller/route/RoutesMap.java
@@ -21,6 +21,7 @@ public class RoutesMap {
         routes.put("admin/manage-inventory", "admin/manageInventory-view.fxml");
         routes.put("admin/manage-settings", "admin/manageSettings-view.fxml");
         routes.put("admin/manage-reports", "admin/manageReports-view.fxml");
+        routes.put("admin/analytics", "admin/Analytics-view.fxml");
 
     }
 
